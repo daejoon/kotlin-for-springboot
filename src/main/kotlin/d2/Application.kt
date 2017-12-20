60038218df371fdf6503f893448b2983bb953852
daejoon.ko <kkode1911@gmail.com>
2017-12-20 10:09:11 +0900

    RestController 추가

--- src/main/kotlin/d2/Application.kt
+++ src/main/kotlin/d2/Application.kt
@@ -4,12 +4,14 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * @Author kdj
  * @Date 2017. 12. 19.
  */
-
+@RestController
 @SpringBootApplication
 class Application : CommandLineRunner {
     private val log = LoggerFactory.getLogger(Application::class.java)
@@ -17,6 +19,9 @@ class Application : CommandLineRunner {
     override fun run(vararg args: String?) {
         log.info("Hello Kotlin")
     }
+
+    @RequestMapping(value = arrayOf("", "/"))
+    fun hello() = "Kotlin for Spring boot"
 }
 
 fun main(args: Array<String>) {
