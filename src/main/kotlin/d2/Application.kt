23be193c0ccb48b0ed9e2ecb4440beac8758f286
daejoon.ko <kkode1911@gmail.com>
2018-02-11 01:04:28 +0900

    kotlin version 1.2.20 -> 1.2.21 Spring boot 1.5.9.RELEASE -> 1.5.10.RELEASE

--- src/main/kotlin/d2/Application.kt
+++ src/main/kotlin/d2/Application.kt
@@ -5,7 +5,9 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
 
 /**
  * @Author kdj
@@ -16,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController
 class Application : CommandLineRunner {
     private val log = LoggerFactory.getLogger(Application::class.java)
 
+    val counter = AtomicLong()
+
     override fun run(vararg args: String?) {
         log.info("Hello Kotlin")
     }
 
     @GetMapping(value = arrayOf("", "/"))
     fun hello() = "Kotlin for Spring boot"
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String)
+            = Greeting(counter.incrementAndGet(), "Hello $name")
 }
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/d2/Greeting.kt
@@ -0,0 +1,7 @@
+package d2
+
+/**
+ * @Author kdj
+ * @Date 2018. 2. 11.
+ */
+data class Greeting(val id: Long, val content: String)
